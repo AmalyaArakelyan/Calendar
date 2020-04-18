e693556f1fdf26a5e065b616a1e3a538d9c9cd5b
Amalya <amalyaarakelyana@gmail.com>
2020-04-18 13:09:08 +0400

    confirm modals in Actions

--- src/components/itemList/Actions.js
+++ src/components/itemList/Actions.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {connect} from "react-redux";
-
+//Components
+import Modal from "../elements/Modal"
 function Actions(props) {
     const {item} = props
-    
+    const [openDelete, setOpenDelete] = useState(false)
+    const [openDone, setOpenDone] = useState(false)
     const delateItem = () => {
 
     }
@@ -11,23 +13,36 @@ function Actions(props) {
     const changeStatus = () => {
       
     }
-   
+    const openDeleteDialog = () => setOpenDelete(!openDelete)
+    const openDoneDialog = () => setOpenDone(!openDone)
 
     return (
       <>
         <button 
-          onClick={delateItem} 
+          onClick={openDeleteDialog} 
           type="button" 
           className="btn btn-outline-danger">
             delete
         </button>
         <hr />
         <button 
-          onClick={changeStatus} 
+          onClick={openDoneDialog} 
           type="button" 
           className="btn btn-primary">
             Make as done
         </button>
+        <Modal 
+          title="Are you sure you want to delete this item" 
+          butonText="Delete"  
+          submit={delateItem} 
+          show={openDelete} 
+          onHide={openDeleteDialog}/>
+        <Modal 
+          title="Change status to done" 
+          butonText="Make Done"  
+          submit={changeStatus} 
+          show={openDone} 
+          onHide={openDoneDialog}/>
       </>
     );
 }
