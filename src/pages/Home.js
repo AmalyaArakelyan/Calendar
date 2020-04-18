cc57df3427563aa16a691573600b320ccff725a5
Amalya <amalyaarakelyana@gmail.com>
2020-04-18 23:12:17 +0400

    create calendar component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+//Components
+import Calendar from '../components/calendar/Calendar'
 
 export default function Home() {
 
   return (
     <div id="home">
-      Home
+      <Calendar/>
     </div>
   );
 }
